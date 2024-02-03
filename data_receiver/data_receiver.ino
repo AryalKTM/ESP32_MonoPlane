bf2242ce0dba50c7d10d1049de82dcd7169478ec
Bipashree Aryal <aryalbipashree@gmail.com>
2024-02-03 18:46:05 +0545

    Added Servo Controls

--- data_receiver/data_receiver.ino
+++ data_receiver/data_receiver.ino
@@ -1,37 +1,46 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <ESP32Servo.h>
 
-typedef struct message{
-  char a[32];
-  int b;
-  float c;
-  bool d;
+#define SERVO1_PIN 25
+#define SERVO2_PIN 26
+#define MOTOR_PIN 33
+
+typedef struct message {
+  int servo1_angle;
+  int servo2_angle;
+  bool motor_status;
 } message;
+
+Servo servo1;
+Servo servo2;
 
 message myData;
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.print("Bytes Received: ");
   Serial.println(len);
-
-  Serial.print("Char: ");
-  Serial.println(myData.a);
-
-  Serial.print("Int: ");
-  Serial.println(myData.b);
-
-  Serial.print("Float: ");
-  Serial.println(myData.c);
-
-  Serial.print("Bool: ");
-  Serial.println("myData.d");
+  Serial.println();
+  Serial.print("Servo 1 Angle: ");
+  Serial.println(myData.servo1_angle);
+  servo1.write(myData.servo1_angle);
+  Serial.print("Servo 2 Angle: ");
+  Serial.println(myData.servo2_angle);
+  servo2.write(myData.servo2_angle);
+  Serial.print("Motor Status: ");
+  Serial.print(myData.motor_status);
+  if (myData.motor_status == true) {
+    digitalWrite(MOTOR_PIN, HIGH);
+  } else {
+    digitalWrite(MOTOR_PIN, LOW);
+  }
   Serial.println();
 }
 
 void setup() {
   Serial.begin(115200);
-
+  pinMode(MOTOR_PIN, OUTPUT);
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != ESP_OK) {
@@ -39,9 +48,14 @@ void setup() {
     return;
   }
 
+  servo1.attach(SERVO1_PIN);
+  servo2.attach(SERVO2_PIN);
+
   esp_now_register_recv_cb(OnDataRecv);
+
+  servo1.write(myData.servo1_angle);
+  servo2.write(myData.servo2_angle);
 }
 
 void loop() {
-
 }
--- data_sender/data_sender.ino
+++ data_sender/data_sender.ino
@@ -1,13 +1,21 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <ezButton.h>
 
-uint8_t broadcastAddress[] = {0xC8, 0xF0, 0x9E, 0x4E, 0x08, 0x3C};
+#define BUTTON1_PIN 21
+#define BUTTON2_PIN 19
+#define BUTTON3_PIN 27
 
-typedef struct message{
-  char a[32];
-  int b;
-  float c;
-  bool d;
+ezButton button1(BUTTON1_PIN);
+ezButton button2(BUTTON2_PIN);
+ezButton button3(BUTTON3_PIN);
+
+uint8_t broadcastAddress[] = { 0xC8, 0xF0, 0x9E, 0x4E, 0x08, 0x3C };
+
+typedef struct message {
+  int servo1_angle;
+  int servo2_angle;
+  bool motor_status;
 } message;
 
 message myData;
@@ -19,12 +27,12 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Successful" : "Delivery Failed");
 }
 
-void setup(){
+void setup() {
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
 
-  if(esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK) {
     Serial.println("Error Initializing ESP_NOW");
     return;
   }
@@ -42,18 +50,46 @@ void setup(){
 }
 
 void loop() {
-  strcpy(myData.a, "CHAR TEST");
-  myData.b = random(1, 20);
-  myData.c = 1.2;
-  myData.d = false;
+  button1.loop();
+  button2.loop();
 
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  if (button1.isPressed()) {
+    if (myData.servo1_angle == 0) {
+      myData.servo1_angle = 180;
+    } else if (myData.servo1_angle == 180) {
+      myData.servo1_angle = 0;
+    }
+  }
 
+  if (button2.isPressed()) {
+    if (myData.servo2_angle == 0) {
+      myData.servo2_angle = 180;
+    } else if (myData.servo2_angle == 180) {
+      myData.servo2_angle = 0;
+    }
+  }
+
+  if (button3.isPressed()) {
+    if (myData.motor_status == true) {
+      myData.motor_status == false;
+    } else if (myData.motor_status == false) {
+      myData.motor_status == true;
+    }
+  }
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
   if (result == ESP_OK) {
     Serial.println("Sent with Success");
-  }
-  else {
-  Serial.println("Error Sending Data");
+    Serial.println("Servo 1 Angle: ");
+    Serial.print(myData.servo1_angle);
+    Serial.println();
+    Serial.println("Servo 2 Angle: ");
+    Serial.print(myData.servo2_angle);
+    Serial.println();
+    Serial.println("Motor Status: ");
+    Serial.print(myData.motor_status);
+  } else {
+    Serial.println("Error Sending Data");
   }
   delay(2000);
 }
